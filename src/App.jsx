a8c1b194f0501bddccd854cc4291a65b0fa3d9be
Amit-AK-07 <amitkoyal2000@gmail.com>
2025-05-23 15:45:56 +0530

    To-Do App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './components/Todo'
+
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid text-white py-4 h-screen'>
+      <Todo />
+    </div>
+  )
+}
+
+export default App
